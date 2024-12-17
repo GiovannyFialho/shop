d33378e14e327ef0b5bf3f9f61ef4d0c26e873a3
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2024-12-17 10:22:18 -0300

    criacao da pagina de produto com dados reais

--- app/components/Product/index.tsx
+++ app/components/Product/index.tsx
@@ -1,24 +1,37 @@
 "use client";
 
+import Image from "next/image";
+
 interface ProductProps {
-  id: string;
+  product: {
+    id: string;
+    name: string;
+    description: string | null;
+    imageUrl: string;
+    price: string | number;
+  };
 }
 
-export function Product({ id }: ProductProps) {
+export function Product({ product }: ProductProps) {
   return (
     <main className="mx-auto my-0 grid max-w-[1180px] grid-cols-2 items-stretch gap-16 pb-8">
-      <div className="flex h-[calc(600px-0.5rem)] w-full max-w-[576px] items-center justify-center rounded-lg bg-custom-gradient p-1"></div>
+      <div className="flex h-[calc(600px-0.5rem)] w-full max-w-[576px] items-center justify-center rounded-lg bg-custom-gradient p-1">
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          width={520}
+          height={480}
+          className="object-cover"
+        />
+      </div>
 
       <div className="flex flex-col">
-        <h1 className="text-2xl text-gray-300">Camiseta {id}</h1>
-        <span className="mt-4 block text-2xl text-green-300">R$ 80,00</span>
+        <h1 className="text-2xl text-gray-300">{product.name}</h1>
+        <span className="mt-4 block text-2xl text-green-300">
+          {product.price}
+        </span>
 
-        <p className="mt-10 text-base text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia vitae
-          dicta quas consequatur repellat laudantium, iusto vero! Atque
-          repellat, tempora fugiat accusantium, temporibus qui quisquam minus
-          odio laborum, perferendis fugit!
-        </p>
+        <p className="mt-10 text-base text-gray-300">{product.description}</p>
 
         <button className="mt-auto cursor-pointer rounded-lg border-0 bg-green-500 p-5 text-base font-bold text-white transition-all duration-300 hover:bg-green-700">
           Comprar agora
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,3 +1,7 @@
+import Stripe from "stripe";
+
+import { stripe } from "@/app/lib/stripe";
+
 import { Product } from "@/app/components/Product";
 
 interface MetadataProps {
@@ -13,8 +17,33 @@ export async function generateMetadata({ params }: MetadataProps) {
   };
 }
 
-export default function ProductPage({ params }: { params: { id: string } }) {
+export default async function ProductPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const { id } = params;
 
-  return <Product id={id} />;
+  const product = await stripe.products.retrieve(id, {
+    expand: ["default_price"],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return (
+    <Product
+      product={{
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        imageUrl: product.images[0],
+        price: price.unit_amount
+          ? new Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            }).format(price.unit_amount / 100)
+          : 0,
+      }}
+    />
+  );
 }
